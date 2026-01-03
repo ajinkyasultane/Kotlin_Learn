f63873a1cf88f966780e5766209c514a16a23447
Ajinkya <ajinkyasultane>
2026-01-03 17:59:04 +0530

    Day one Kotlin Learning

---
+++ src/main/kotlin/Variables.kt
@@ -0,0 +1,63 @@
+import kotlin.math.min
+
+fun main()
+{
+    println("Hello Ajinkya Sultane");
+
+    var a = 10;
+    println(a);
+
+    var username: String = "Ajinkya";
+
+    username = "Sultane";
+    println("Hello " + username);
+
+    var age: Int = 22;
+    println("Hello Ajinkya Sultane your age is $age")
+
+    var ages = 22.5;
+
+    val integermaxvalue = Int.MAX_VALUE;
+    val interminvalue = Int.MIN_VALUE;
+
+    println("Interger mmaxvalue is $integermaxvalue")
+    println("Interger minvalue is $interminvalue")
+
+
+    val bytemaxvalue: Byte = Byte.MAX_VALUE
+    val byteminvalue: Byte = Byte.MIN_VALUE
+
+    println("Byte max value $bytemaxvalue")
+    println("Byte min value $byteminvalue ")
+
+    val shortmaxvalue: Short = Short.MAX_VALUE
+    val shortminvalue: Short = Short.MIN_VALUE
+
+    println("Short Max value is $shortmaxvalue")
+    println("Short min value is $shortminvalue")
+
+    val longmaxvalue: Long = Long.MAX_VALUE
+    val longminvalue: Long = Long.MIN_VALUE
+
+    println("Long Max value is $longmaxvalue")
+    println("Long min value is $longminvalue")
+
+    val doublemaxvalue: Double = Double.MAX_VALUE
+    val doubleminvalue: Double = Double.MIN_VALUE
+
+    println("Double max value is $doublemaxvalue")
+    println("Double min value is $doubleminvalue")
+
+    val floatmaxvalue: Float = Float.MAX_VALUE
+    val floatminvalue: Float = Float.MIN_VALUE
+
+    println("FLoat max value is $floatmaxvalue")
+    println("Float min value is $floatminvalue")
+
+
+    val  charmaxvalue: Char = Char.MAX_VALUE
+    val charminvalue: Char = Char.MIN_VALUE
+
+    println("Char Max value is $charmaxvalue")
+    println("Char min value is $charminvalue")
+}
